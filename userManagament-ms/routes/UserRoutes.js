5a63aeb7f13480b3e109c56d82da628e794abb85
NaserKh <naserK@360medlink.com>
2023-06-22 16:47:11 +0100

    [backend]make routes

--- userManagament-ms/routes/UserRoutes.js
+++ userManagament-ms/routes/UserRoutes.js
@@ -1,12 +1,20 @@
-const userController = require('../controllers/UserController');
-const { AddUserValidation } =  require('../middlewares/Validation');
-const { upload } =require('../middlewares/Upload');
-
 const router = require('express').Router();
-
-
-router.post('/addUser', upload, AddUserValidation,userController.createUser);
-router.get('/users',userController.getAllUser);
-router.get('/user/:id',userController.getUser);
-
+const authController = require('../controllers/authentification/AuthentificationController');
+const userController = require('../controllers/admin/UserController');
+const uploadController = require('../controllers/user/userController');
+const multer = require('multer');
+const upload = multer();
+//auth
+router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
+//user:DB
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userinfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
+//upload
+router.post('/upload', upload.single('file'), uploadController.uploadProfil);
 module.exports = router;
