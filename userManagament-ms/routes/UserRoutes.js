f5147a638b51ae1ba85887a63d9971afa7b85357
NaserKh <naserK@360medlink.com>
2023-06-22 22:12:05 +0100

    [backend] refacotrying

--- userManagament-ms/routes/UserRoutes.js
+++ userManagament-ms/routes/UserRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const authController = require('../controllers/authentification/AuthentificationController');
-const userController = require('../controllers/admin/UserController');
-const uploadController = require('../controllers/user/userController');
-const { checkUser, requireAuth } = require('../middlewares/AuthentificationMiddleware');
+const adminController = require('../controllers/admin/UserController');
+const userController = require('../controllers/user/userController');
+const {  requireAuth } = require('../middlewares/AuthentificationMiddleware');
 
 const multer = require('multer');
 const upload = multer();
@@ -14,12 +14,12 @@ router.get("/logout", authController.logout);
 // Grouped routes with requireAuth middleware
 router.use(requireAuth);
 //user:DB
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.userinfo);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/", adminController.getAllUsers);
+router.get("/:id", adminController.userinfo);
+router.put("/:id", adminController.updateUser);
+router.delete("/:id", adminController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
 //upload
-router.post('/upload', upload.single('file'), uploadController.uploadProfil);
+router.post('/upload', upload.single('file'), userController.uploadProfil);
 module.exports = router;
