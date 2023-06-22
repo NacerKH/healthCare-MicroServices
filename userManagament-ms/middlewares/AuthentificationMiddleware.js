fe42d5d866ad2498fc46949d053737905af17156
NaserKh <naserK@360medlink.com>
2023-06-22 16:46:23 +0100

    [backend]middleware check auth

---
+++ userManagament-ms/middlewares/AuthentificationMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../Models/User');
+
+module.exports.checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err) {
+
+                res.locals.user = null;
+                res.cookie('jwt', '', { maxAge: 1 });
+                next();
+            } else {
+                console.log('decoded token ' + decodedToken.id);
+                let user = await UserModel.findById(decodedToken.id);
+                res.locals.user = user;
+                console.log(res.locals.user);
+
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+
+
+}
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log(err);
+                res.send(200).json('No token')
+            }
+            else {
+                console.log(decodedToken.id);
+                next();
+            }
+        });
+    } else {
+        console.log('No token');
+    }
+}
