5b2af8cdcdd8b107a4977937e01921141f35bbdb
NaserKh <mednaceur199530@gmail.com>
2023-06-29 17:48:33 +0100

    [BE] add verfication email  route and apply middlaware

--- userManagament-ms/routes/UserRoutes.js
+++ userManagament-ms/routes/UserRoutes.js
@@ -2,22 +2,34 @@ const router = require('express').Router();
 const authController = require('../controllers/authentification/AuthentificationController');
 const adminController = require('../controllers/admin/UserController');
 const userController = require('../controllers/user/userController');
-const {  requireAuth } = require('../middlewares/AuthentificationMiddleware');
+const EmailVerficationController = require('../controllers/user/EmailVerficationController');
+
+const {  requireAuth ,checkUser} = require('../middlewares/AuthentificationMiddleware');
+const   checkEmailVerification = require('../middlewares/MustVerified');
 
 const multer = require('multer');
 const upload = multer();
 //auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
-router.get("/logout", authController.logout);
+
 
 // Grouped routes with requireAuth middleware
 router.use(requireAuth);
+router.use(checkUser);
+router.post("/email/send-email-verification", EmailVerficationController.sendEmailVerification);
+router.get("/email/verify-email/:verificationToken", EmailVerficationController.verificationEmail);
+
+router.use(checkEmailVerification);
+router.get("/logout", authController.logout);
 //user:DB
 router.get("/", adminController.getAllUsers);
 router.get("/:id", adminController.userinfo);
 router.put("/:id", adminController.updateUser);
+router.post("/addUser", adminController.addUser);
 router.delete("/:id", adminController.deleteUser);
+
+//user:follow 
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
 //upload
