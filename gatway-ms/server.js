d96e232045e233a590fb21e62102ec4b9bced555
NaserKh <mednaceur199530@gmail.com>
2023-07-19 03:48:15 +0100

    add looging

--- gatway-ms/server.js
+++ gatway-ms/server.js
@@ -1,16 +1,25 @@
-const express= require('express');
-require('dotenv').config({path: './config/.env'});
-const db =require('./config/db');
-const userRoutes=require('./routes/UserRoutes')
+const express = require('express');
+require('dotenv').config({ path: './config/.env' });
+const cookieParser = require('cookie-parser');
+
+const { setupLogging } = require("./Services/logging");
+const routes = require('./routes')
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/api/v1/',userRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+setupLogging(app);
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! from `gatway`')
+    res.send(req.cookies.user_id)
+})
+app.use(routes)
 
 
 
 ////server
-app.listen(process.env.PORT, () =>{
-    console.log(`listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(` gatway Api listening on port ${process.env.PORT}`);
 })
